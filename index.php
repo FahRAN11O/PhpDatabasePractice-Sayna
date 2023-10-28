f257deb3439ef6f06ee5c2c7652cdbed79d052a0
Fahmeno <famenofa@gmail.com>
2023-10-28 16:36:52 +0300

    insertion d'une valeur dans la base de donnée

--- index.php
+++ index.php
@@ -2,6 +2,20 @@
 include('config.php');
 include('connexion.php');
 
+$query = "INSERT INTO users (login,password) VALUES (:login, :password)";
+$stmt =  $pdo->prepare($query);
+
+$login ='user04';
+$password='user04';
+
+//Liaison des valeurs avec les paramètres
+$stmt->bindParam(':login', $login);
+$stmt->bindParam(':password', $password);
+
+//Execution de la requête
+$stmt->execute();
+
+
 $query = 'SELECT * FROM users';
 $stmt = $pdo->query($query);
 
@@ -9,5 +23,7 @@ $stmt = $pdo->query($query);
 echo '<table>';
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 	//Utiliser les données récupérées
-	echo $row['login'];
+	echo '<tr><td>'.$row['login'].'</td><td>'.$row['password'].'</td></tr>';
 }
+
+echo '</table>';
